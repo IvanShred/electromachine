3437fa96e7e9bd1d9604e8e67ef0e85763f9491f
Кухта Иван <kuhtaia@it2g.tu>
2019-09-29 11:06:11 +0700

    Старт проекта. Добавление changeset-ов liquibase

---
+++ src/main/java/ru/shred/electromachine/ElectromachineApplication.java
@@ -0,0 +1,13 @@
+package ru.shred.electromachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectromachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectromachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/shred/electromachine/ElectromachineApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.shred.electromachine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElectromachineApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
