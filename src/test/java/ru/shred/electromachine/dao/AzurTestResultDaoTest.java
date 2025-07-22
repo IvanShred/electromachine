225a69feeb0f45eaec38ae3f6accbf23e6052ba3
Кухта Иван <kuhtaia@it2g.ru>
2025-07-22 07:09:53 +0300

    Тесты в контейнерах

--- src/test/java/ru/shred/electromachine/dao/AzurTestResultDaoTest.java
+++ src/test/java/ru/shred/electromachine/dao/AzurTestResultDaoTest.java
@@ -5,16 +5,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import ru.shred.electromachine.model.AzurTestResult;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
 @ActiveProfiles("test")
+@Testcontainers
+@SpringBootTest
+@Transactional
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class AzurTestResultDaoTest {
+
+    @Container
+    protected static final PostgreSQLContainer<?> container =
+            new PostgreSQLContainer<>("postgres:16.1")
+                    .withDatabaseName("testdb")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+        registry.add("spring.liquibase.url", container::getJdbcUrl);
+        registry.add("spring.liquibase.user", container::getUsername);
+        registry.add("spring.liquibase.password", container::getPassword);
+    }
 
     @Autowired
     private AzurTestResultDao dao;
--- src/test/java/ru/shred/electromachine/dao/UserDaoTest.java
+++ src/test/java/ru/shred/electromachine/dao/UserDaoTest.java
@@ -4,49 +4,43 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 import ru.shred.electromachine.model.User;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
 @ActiveProfiles("test")
-@SqlGroup({
-        @Sql(
-                scripts = "classpath:insert.sql",
-                executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
-        ),
-        @Sql(
-                scripts = "classpath:clear.sql",
-                executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD
-        )
-})
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Testcontainers
+@SpringBootTest
+@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserDaoTest {
 
-    @Autowired
-    private UserDao userDao;
-
-    @ServiceConnection
     @Container
-    static PostgreSQLContainer<?> container = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.1"));
+    protected static final PostgreSQLContainer<?> container =
+            new PostgreSQLContainer<>("postgres:16.1")
+                    .withDatabaseName("testdb")
+                    .withUsername("test")
+                    .withPassword("test");
 
     @DynamicPropertySource
     static void registerProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", container::getJdbcUrl);
         registry.add("spring.datasource.username", container::getUsername);
         registry.add("spring.datasource.password", container::getPassword);
+        registry.add("spring.liquibase.url", container::getJdbcUrl);
+        registry.add("spring.liquibase.user", container::getUsername);
+        registry.add("spring.liquibase.password", container::getPassword);
     }
+
+    @Autowired
+    private UserDao userDao;
 
     @Test
     void getByEmail() {
